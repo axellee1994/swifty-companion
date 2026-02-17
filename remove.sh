98e6a41dabf56b216b7be617530e20424ad2f6c0
Axel Lee <axellee1994@gmail.com>
2026-02-17 19:52:06 +0800

    Created scripts to start and remove

---
+++ remove.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Remove platform-specific folders
+rm -rf android ios linux macos web windows
+
+# Remove the test folder (optional, if you aren't using it yet)
+rm -rf test
+
+# Remove IDE settings and generated files
+rm -rf .idea
+rm -f *.iml
+rm -rf pubspec.lock
+rm -rf analysis_options.yaml
+rm -rf .metadata
+
+echo "Cleaned up 🧹!"
---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# 1. Regenerate platform folders
+flutter create .
+
+# 2. Get dependencies
+flutter pub get
+
+echo "Setup complete! 🚀"
